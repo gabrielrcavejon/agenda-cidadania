56bd0f8b8c126603e1ec6f021dc5e0c4741d78a9
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2025-08-19 20:52:55 -0300

    feat: Ajuste em entidade de banco de dados

--- backend/src/empresa/entities/empresa.entity.ts
+++ backend/src/empresa/entities/empresa.entity.ts
@@ -9,19 +9,32 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+export enum Tipo {
+  ADMINISTRADOR = 'A',
+  ORGANIZADOR = 'O',
+}
+
 @Entity({ name: 'empresa' })
 export class Empresa {
   @PrimaryGeneratedColumn({ name: 'idempresa' })
   idEmpresa: number;
 
-  @Column({ name: 'nome', length: 60 })
+  @Column({ name: 'nome', length: 100 })
   nome: string;
 
-  @Column({ name: 'fantasia', length: 60 })
+  @Column({ name: 'fantasia', length: 100 })
   fantasia: string;
 
   @Column({ name: 'cnpj', length: 14, unique: true })
   cnpj: string;
+
+  @Column({
+    type: 'enum',
+    enum: Tipo,
+    name: 'tipo',
+    default: Tipo.ADMINISTRADOR,
+  })
+  tipo: Tipo;
 
   @OneToMany(() => Usuario, (usuario) => usuario.empresa)
   usuario: Usuario[];
